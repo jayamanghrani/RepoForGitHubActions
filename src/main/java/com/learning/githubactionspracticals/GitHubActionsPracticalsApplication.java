7eebb79b53da0c8afd99c143d375c2a48a78c2f5
JayaOwner <jayamanghrani@gmail.com>
2026-07-05 13:44:51 +0530

    Initial commit

---
+++ src/main/java/com/learning/githubactionspracticals/GitHubActionsPracticalsApplication.java
@@ -0,0 +1,13 @@
+package com.learning.githubactionspracticals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionsPracticalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubActionsPracticalsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/githubactionspracticals/GitHubActionsPracticalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.githubactionspracticals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionsPracticalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

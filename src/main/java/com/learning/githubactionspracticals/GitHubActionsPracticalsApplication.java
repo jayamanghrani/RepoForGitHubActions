1b4293eacb7525309c6c89539a21d48c1a4a01e2
JayaOwner <jayamanghrani@gmail.com>
2026-07-09 21:36:46 +0530

    Different runner test

--- src/main/java/com/learning/githubactionspracticals/GitHubActionsPracticalsApplication.java
+++ src/main/java/com/learning/githubactionspracticals/GitHubActionsPracticalsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitHubActionsPracticalsApplication {
 
     public static void main(String[] args) {
-        System.out.println("GitHubActions");
+        System.out.println("GitHubActions...");
         SpringApplication.run(GitHubActionsPracticalsApplication.class, args);
     }
 
